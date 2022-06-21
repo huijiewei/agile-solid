a695a1fd35682e4c0c0d12e146ad4159cd536503
Huijie Wei <huijiewei@outlook.com>
2022-06-21 21:02:14 +0800

    chore: 使用环境变量确定是否使用打包分析

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,28 +1,33 @@
 import { resolve } from 'path';
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-export default defineConfig({
-  plugins: [solidPlugin(), splitVendorChunkPlugin()],
-  build: {
-    rollupOptions: {
-      plugins: [
-        visualizer({
-          open: true,
-          filename: './node_modules/.cache/visualizer/stats.html',
-          gzipSize: true,
-          brotliSize: true,
-        }),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [solidPlugin(), splitVendorChunkPlugin()],
+    build: {
+      rollupOptions: {
+        plugins: [
+          env['BUNDLE_ANALYZE'] == '1' &&
+            visualizer({
+              open: true,
+              filename: './node_modules/.cache/visualizer/stats.html',
+              gzipSize: true,
+              brotliSize: true,
+            }),
+        ],
+      },
+    },
+    resolve: {
+      alias: [
+        {
+          find: /^@agile-solid\/(.*)$/,
+          replacement: resolve(__dirname, '../packages/$1/src'),
+        },
       ],
     },
-  },
-  resolve: {
-    alias: [
-      {
-        find: /^@agile-solid\/(.*)$/,
-        replacement: resolve(__dirname, '../packages/$1/src'),
-      },
-    ],
-  },
+  };
 });
