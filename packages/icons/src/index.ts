dd55f0e5a1dc4ed22dafe345b78d0cb9efc29c0b
Huijie Wei <huijiewei@outlook.com>
2022-06-25 12:45:57 +0800

    feat: 增加 Menu, Close 图标

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -4,3 +4,5 @@ export * from './icons/Moon';
 export * from './icons/Sun';
 export * from './icons/Github';
 export * from './icons/Clipboard';
+export * from './icons/Menu';
+export * from './icons/Close';
